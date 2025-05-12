3649faa5564248c50b0e6c2f88e65e39d4aef341
krishnadale007 <152286255+krishnadale007@users.noreply.github.com>
2025-05-12 19:04:59 +0000

    vpc

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,11 @@ resource "aws_vpc" "my_vpc" {
     Name = "my_vpc"
   }
 }
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
